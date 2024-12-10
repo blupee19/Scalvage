56dd7bcf60a837afbfc1d5cc6d644c2bd9f7a4e3
Rachit Sharma <rachitsharma190904@gmail.com>
2024-12-10 20:49:55 +0530

    weapon

--- Assets/Scripts/WeaponAttack.cs
+++ Assets/Scripts/WeaponAttack.cs
@@ -1,18 +1,47 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    public float offset = 2f;
+    public GameObject weapon;
+    public PlayerController controller;
+    public InputActionAsset playerControls;
+    private InputAction closeAttackAction;
+    public float offset = 0f;
+    public bool closeAttack {  get; private set; }
+
+    private void Awake()
+    {
+        closeAttackAction = playerControls.FindActionMap("Player").FindAction("Attack");
+        closeAttackAction.performed += context => closeAttack = true;
+        closeAttackAction.canceled += context => closeAttack = false;
+    }
+
+    private void OnEnable()
+    {
+        closeAttackAction.Enable();
+    }
+    private void OnDisable()
+    {
+        closeAttackAction.Disable();
+    }
 
     private void Update()
     {
         Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-
         difference.Normalize();
-
         float rotation_z = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-
         transform.rotation = Quaternion.Euler(0f, 0f, rotation_z + offset);
+
+        if (!controller.facingRight)
+        {
+            weapon.GetComponent<SpriteRenderer>().flipY = true;
+        }
+        else 
+        {
+            weapon.GetComponent<SpriteRenderer>().flipY = false;
+        }
+
 
     }
 
