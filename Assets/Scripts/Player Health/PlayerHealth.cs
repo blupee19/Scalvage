81827977bc4fbf93e4ff8443ef4619949e27cde4
Rachit Sharma <rachitsharma190904@gmail.com>
2024-12-14 23:00:19 +0530

    s

--- Assets/Scripts/Player Health/PlayerHealth.cs
+++ Assets/Scripts/Player Health/PlayerHealth.cs
@@ -1,9 +1,11 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem.Utilities;
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private float startingHealth;
+    [SerializeField] private Camera mainCamera;
     public float currentHealth { get; private set; }
 
     private void Awake()
@@ -30,7 +32,29 @@ public class PlayerHealth : MonoBehaviour
         if (other.gameObject.CompareTag("Enemy"))
         {
             TakeDamage(1f);
+            StartCoroutine(Shake(0.2f,0.2f));
+
             Debug.Log("Hit!!!");
         }
+    }
+
+
+    public IEnumerator Shake(float duration, float magnitude)
+    {
+        Vector3 originalPosition = mainCamera.transform.localPosition;
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            float offsetX = Random.Range(-1f, 1f) * magnitude;
+            float offsetY = Random.Range(-1f, 1f) * magnitude;
+
+            mainCamera.transform.localPosition = new Vector3(originalPosition.x + offsetX, originalPosition.y + offsetY, originalPosition.z);
+
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        mainCamera.transform.localPosition = originalPosition;
     }
 }
