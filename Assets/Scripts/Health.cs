fa1942f46d183de45d38a4c4f3dfa0bd0a596759
shefali-dewangan <shefalidewangan3@gmail.com>
2024-12-14 17:12:40 +0530

    EyeEnemyAI

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,9 +5,22 @@ public class Health : MonoBehaviour
 {
     [SerializeField] private int currentHealth, maxHealth;
     public UnityEvent<GameObject> OnHitWithReference, OnDeathWithReference;
-    public GameObject Enemy;
+    public GameObject EnemyEye;
     [SerializeField] private bool isDead = false;
+    [SerializeField] private float destroyDelay = 2f;
+    private Rigidbody2D rb;
+    public CircleCollider2D enemyCollider;
 
+    [SerializeField] private float hitForce, hitTorque;
+
+    private EnemyEyeAI enemyAI; 
+
+
+    void Start()
+    {
+        rb = EnemyEye.GetComponent<Rigidbody2D>();
+        enemyAI = GetComponent<EnemyEyeAI>();
+    }
     public void InitializeHealth(int healthValue)
     {
         currentHealth = healthValue;
@@ -19,7 +32,7 @@ public class Health : MonoBehaviour
     {
         if (isDead)
             return;
-        if(sender.layer == gameObject.layer) 
+        if (sender.layer == gameObject.layer)
             return;
 
         currentHealth -= amount;
@@ -27,13 +40,59 @@ public class Health : MonoBehaviour
         {
             OnHitWithReference?.Invoke(sender);
         }
-        else 
+        else
         {
             OnDeathWithReference?.Invoke(sender);
             isDead = true;
-            Destroy(Enemy);
+
+            DisableAI();
+
+            EnableGravity();
+            RollAfterDeath(sender);
+            Invoke(nameof(DestroyEnemy), destroyDelay);
         }
 
 
+    }
+
+    private void EnableGravity()
+    {
+
+        if (currentHealth <= 0)
+        {
+            rb.gravityScale = 9;
+            enemyCollider.isTrigger = false;
+            rb.freezeRotation = false;
+
+        }
+    }
+
+    private void DestroyEnemy()
+    {
+        Destroy(EnemyEye);
+    }
+
+    private void RollAfterDeath(GameObject sender)
+    {
+        Vector2 hitDirection = (EnemyEye.transform.position - sender.transform.position).normalized;
+
+        rb.AddForce(hitDirection * hitForce, ForceMode2D.Impulse);
+
+        if(hitDirection.x > 0)
+        {
+            rb.AddTorque(-hitTorque, ForceMode2D.Impulse);
+        }
+        else
+        {
+            rb.AddTorque(hitTorque, ForceMode2D.Impulse);
+        }
+           
+
+
+    }
+
+    private void DisableAI()
+    {
+        enemyAI.enabled = false;              
     }
 }
