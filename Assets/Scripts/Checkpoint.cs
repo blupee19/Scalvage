343d82e1ff90c99b871daf77a001213b0ddf822d
Rachit Sharma <rachitsharma190904@gmail.com>
2024-12-14 15:34:00 +0530

    checkpoint

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    private Respawn respawn;
+    private BoxCollider2D boxCollider;
+
+    void Awake()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();   
+        respawn = GameObject.FindGameObjectWithTag("Respawn").GetComponent<Respawn>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            respawn.respawnPoint = this.gameObject;
+            boxCollider.enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject respawnPoint;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            player.transform.position = respawnPoint.transform.position;
+        }
+    }
+}
