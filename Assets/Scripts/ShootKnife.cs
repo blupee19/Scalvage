46942a177641358c89ede24f22176552b60aa8b9
Rachit Sharma <rachitsharma190904@gmail.com>
2024-12-16 14:25:56 +0530

    dunno

--- Assets/Scripts/ShootKnife.cs
+++ Assets/Scripts/ShootKnife.cs
@@ -34,7 +34,7 @@ public class ShootKnife : MonoBehaviour
         Destroy(gameObject, 5);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
