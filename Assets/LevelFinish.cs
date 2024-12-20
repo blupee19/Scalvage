60c4fd9e6aa0e20563df8101fc62ec4a6d5b06ba
Rachit Sharma <rachitsharma190904@gmail.com>
2024-12-21 01:14:23 +0530

    oh my god

---
+++ Assets/LevelFinish.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelFinish : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            StartCoroutine(WaitForLevelChange());
+        }
+    }
+
+    private IEnumerator WaitForLevelChange()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadSceneAsync(2);
+        
+    }
+}
